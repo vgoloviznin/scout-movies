add29711e36194795bf3b46e5f53caa0cb7e4fd8
Vsevolod Goloviznin <vsevolod.goloviznin@gmail.com>
2019-09-01 00:36:49 +0300

    - added tests for Actor resolver

---
+++ test/data/fakes.js
@@ -0,0 +1,20 @@
+const faker = require('faker');
+
+module.exports = {
+  actor: {
+    name: faker.name.findName(),
+    birthday: faker.date.past(),
+    country: faker.random.word(),
+    id: faker.random.number()
+  },
+  directors: Array(...Array(10)).map(() => ({
+    name: faker.name.findName(),
+    birthday: faker.date.past(),
+    country: faker.random.word()
+  })),
+  movies: Array(...Array(10)).map(() => ({
+    title: faker.random.words(),
+    year: faker.random.number({ min: 1900, max: 2019 }),
+    rating: faker.random.number({ min: 1, max: 5, precision: 0.5 }),
+  }))
+};
